3b1a6d4030d87b20f77a2541ec9b04e3b3822c58
Kanelo99 <kanelo.lekena99@gmail.com>
2023-02-16 11:51:28 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
