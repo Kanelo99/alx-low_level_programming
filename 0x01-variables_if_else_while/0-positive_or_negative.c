f6cab9892e76d37a1a3661805d90750460529748
Kanelo99 <kanelo.lekena99@gmail.com>
2023-02-17 10:09:49 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,16 +3,20 @@
 #include <time.h>
 
 /*HEADER*/
+
 /*FUNCTION*/
 
 /**
  * main - Entry Point
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
