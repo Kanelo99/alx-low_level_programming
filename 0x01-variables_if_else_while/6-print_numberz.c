234d962022e156275fdee51ed5a1b53f6b8a8577
Kanelo99 <kanelo.lekena99@gmail.com>
2023-02-17 12:45:43 -0800

    6-numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	number = 0;
+	while (number < 10)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
